35172c5e36b8c94b0797d2c73338771ecc18e53f
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2026-02-23 15:48:46 +0100

    feat: add file reading and HuffmanTree struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+struct HuffmanTree {
+    left: u8,
+    right: Box<HuffmanTree>,
+}
+
+fn read_input(path: &str) -> Vec<u8> {
+    std::fs::read(path).expect("failed to read file")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    let bytes = read_input(path);
+    println!("{}", String::from_utf8_lossy(&bytes));
 }
